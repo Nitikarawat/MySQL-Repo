0ec53a6f802d4811d0e72f7ef2e98115e1c62dd9
Nitika Rawat <73019582+Nitikarawat@users.noreply.github.com>
2026-05-02 11:50:19 +0530

    Add alternative age calculation method in SQL
    Added an alternative method to calculate age based on current year.

--- MyPersonal Use Scripts/find_age_from_dob.sql
+++ MyPersonal Use Scripts/find_age_from_dob.sql
@@ -1,1 +1,3 @@
 select name, floor(datediff(curdate(), dob)/365) as age from employee_details;
+-- based on year current year and dob year ke basis pe
+select name, year(curdate())-year(dob) as age from employee_details;
