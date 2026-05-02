d9ddd9e5e80706c6f609d1fcdf387c7f8a1975fa
Nitika Rawat <73019582+Nitikarawat@users.noreply.github.com>
2026-05-02 18:42:40 +0530

    Create dynamic sql.sql

---
+++ MyPersonal Use Scripts/dynamic sql.sql
@@ -0,0 +1,20 @@
+USE `sql_hr`;
+DROP procedure IF EXISTS `new_procedure`;
+
+USE `sql_hr`;
+DROP procedure IF EXISTS `sql_hr`.`new_procedure`;
+;
+
+DELIMITER $$
+USE `sql_hr`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `new_procedure`(limitval integer)
+BEGIN
+set @l=limitval;
+prepare stmt from 'select * from sql_hr.employees limit ?';
+
+execute stmt using @l;
+END$$
+
+DELIMITER ;
+;
+
