8031d24cf4c5799da07917276aefd21e9c270f7a
Nitika Rawat <73019582+Nitikarawat@users.noreply.github.com>
2026-05-02 19:54:31 +0530

    Update highest salary in each department.sql

--- MyPersonal Use Scripts/highest salary in each department.sql
+++ MyPersonal Use Scripts/highest salary in each department.sql
@@ -1,2 +1,5 @@
+-- number of employee in each dept
+select office_id as department_id, count(*) as 'number of employee' from employees group by office_id;
+-- highest salary in each dept
 select office_id as department_id, max(salary) as highest_salary, count(salary)  from employees group by office_id;
 select * from employees;
