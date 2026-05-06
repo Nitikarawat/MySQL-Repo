7297d56184e4e0c5815553aa9adb2a99c60947f2
Nitika Rawat <73019582+Nitikarawat@users.noreply.github.com>
2026-05-06 11:35:55 +0530

    Update create duplicate table structure from existing table structure.sql

--- MyPersonal Use Scripts/create duplicate table structure from existing table structure.sql
+++ MyPersonal Use Scripts/create duplicate table structure from existing table structure.sql
@@ -1,3 +1,7 @@
+-- create table structure from existing table
 create table if not exists sql_hr.dup_employees2 like sql_hr.employees2;
 select * from sql_hr.employees2;
 SELECT * FROM sql_hr.dup_employees2;
+
+-- create table with data as well
+create table if not exists sql_hr.dup_employees2 as select * from  sql_hr.employees2;
